950fe2ee5fb07a962c00e59cf439f4fc0c2e1904
Allen Kerr <alkerr@gmail.com>
2019-04-06 15:51:00 -0700

    things

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ require 'sequel'
 ENV['RACK_ENV'] = 'test'
 config_file_path = File.read(File.join(File.dirname(__FILE__), '../config/database.yml'))
 database_creds = YAML.safe_load(config_file_path, [], [], true)
-database = database_creds['dj_scheduler_db_test']
+database = ENV['DATABASE_URL'] || database_creds['dj_scheduler_db_test']
 
 DB = Sequel.connect(database)
 
